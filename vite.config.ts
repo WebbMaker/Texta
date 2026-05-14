9bab47721c420691384d7ef9bf19c64818a1ccf5
WebbMaker <cynhyxt2yg@privaterelay.appleid.com>
2026-05-14 02:55:20 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    react()
-  ],
-  base: '/Tori/',
+  plugins: [react()],
+  // 'base' musi odpowiadać nazwie Twojego repozytorium na GitHubie
+  base: '/Texta/', 
 })
