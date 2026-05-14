a6011deeb4235a1db859268b632bb6f16df270a1
WebbMaker <cynhyxt2yg@privaterelay.appleid.com>
2026-05-14 03:18:13 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // 'base' musi odpowiadać nazwie Twojego repozytorium na GitHubie
-  base: '/Texta/', 
+  base: '/Texta/',
 })
